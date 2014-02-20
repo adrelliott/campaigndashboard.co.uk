db54ee313d32918ee76fc63e3958c6b14231cc16
Al Elliott <al@dallasmatthews.co.uk>
2014-02-20 11:43:27 +0100

    Updated env vars

--- app/config/database.php
+++ app/config/database.php
@@ -57,8 +57,7 @@ return array(
 			'prefix'   => '',
 		),
 
-        /* Datatbase settings for fortrabbit */
-		'mysql' => array(
+          'mysql' => array(
             'driver'    => 'mysql',
             'host'      => getenv('DB_HOST'),
             'database'  => getenv('DB_NAME'),
@@ -68,16 +67,17 @@ return array(
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
         ),
-// 'mysql' => array(
-// 			'driver'    => 'mysql',
-// 			'host'      => 'campaigndash.mysql.eu1.frbit.com',
-// 			'database'  => 'campaigndash',
-//             'username'  => 'campaigndash',
-//             'password'  => '4XlcTgajzfTebNwB',
-// 			'charset'   => 'utf8',
-// 			'collation' => 'utf8_unicode_ci',
-// 			'prefix'    => '',
-// 		),
+
+  //       'mysql' => array(
+		// 	'driver'    => 'mysql',
+		// 	'host'      => 'campaigndash.mysql.eu1.frbit.com',
+		// 	'database'  => 'campaigndash',
+  //           'username'  => 'campaigndash',
+  //           'password'  => '4XlcTgajzfTebNwB',
+		// 	'charset'   => 'utf8',
+		// 	'collation' => 'utf8_unicode_ci',
+		// 	'prefix'    => '',
+		// ),
 
 		'pgsql' => array(
 			'driver'   => 'pgsql',
--- app/views/layouts/app.blade.php
+++ app/views/layouts/app.blade.php
@@ -9,33 +9,7 @@
 
         <title>Dashboard</title>
 
-        <!-- Get Bootstrap core CSS -->
-        {{-- HTML::style('//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css') --}}
-        {{ HTML::style('/assets/css/bootstrap/bootstrap.min.css') }}
-
-        <!-- Get other plugin CSS files -->
-        {{-- HTML::style('/assets/css/bootstrap/bootstrap-switch.min.css') --}}
-        {{ HTML::style('/assets/css/flippant/flippant.css') }}
-        {{ HTML::style('/assets/css/bootstrap/bootstrap3-summernote.css') }}
-        {{ HTML::style('/assets/css/bootstrap/summernote.css') }}
-
-        <!-- Include Font-awesome Upgraded from 3.2.1-->
-        {{-- HTML::style('//netdna.bootstrapcdn.com/font-awesome/4.0.0/css/font-awesome.css') --}}
-        {{ HTML::style('//netdna.bootstrapcdn.com/font-awesome/4.0.0/css/font-awesome.css') }}
-
-        <!-- Custom styles for this template -->
-        {{ HTML::style('/assets/css/main.css') }}
-
-
-        {{-- HTML::script('//ajax.googleapis.com/ajax/libs/angularjs/1.2.13/angular.min.js') --}}
-
-        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!--[if lt IE 9]>
-            {{ HTML::script('/assets/js/others/html5shiv.js') }}  
-          <script src="<?php// echo site_url('assets/bootstrap-3/js/respond.min.js'); ?>"></script>
-        <![endif]-->
-
-        <link rel="stylesheet" type="text/css" href="http://services.postcodeanywhere.co.uk/css/captureplus-2.10.min.css?key=xu56-tj37-eb64-kw34" /><script type="text/javascript" src="http://services.postcodeanywhere.co.uk/js/captureplus-2.10.min.js?key=xu56-tj37-eb64-kw34"></script>
+        @include('partials.app._header-development')
         
     </head>
 
@@ -83,31 +57,8 @@
             @include('partials.common._modal', array('modalTitle' => ''))
         @show
         <!-- / Modal -->
-
-        <!-- Call all Frameworks -->
-        {{-- HTML::script('assets/js/bootstrap/wysihtml5-0.3.0_rc2.min.js') --}}
-        {{ HTML::script('//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js') }}
         
-        <!-- Call Core Boostrap JS -->
-        {{-- HTML::script('assets/js/bootstrap/ui-bootstrap-tpls-0.10.0.min.js') --}}
-        {{ HTML::script('//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js') }}
-
-        <!-- Call Other Plugins -->
-        {{-- HTML::script('assets/js/bootstrap/bootstrap-switch.min.js') --}}
-        {{ HTML::script('assets/js/flippant/flippant.min.js') }}
-        {{ HTML::script('assets/js/bootstrap/summernote.min.js') }}
-        
-
-        <!-- Call all datatables JS -->
-        {{ HTML::script('assets/js/datatables/jquery.dataTables.min.js') }}
-        {{ HTML::script('assets/js/datatables/dataTables.bootstrapPagination.js') }}
-        {{ HTML::script('assets/js/datatables/datatables.custom.js') }}
-
-        <!-- Call Other JS -->
-        {{ HTML::script('assets/js/app/app.js') }}
-        {{ HTML::script('assets/js/app/form.js') }}
-        {{ HTML::script('assets/js/app/modal.js') }}
-        {{-- HTML::script('assets/js/app/appAngular.js') --}}
-        
+        @include('partials.app._footer-development')
+        {{ var_dump(App::environment())}}
     </body>
 </html>
