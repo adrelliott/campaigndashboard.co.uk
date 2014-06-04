582f8d38a8ecf4d843ad429fec5fcb1fa3a29ea3
Al Elliott <al@dallasmatthews.co.uk>
2014-06-04 23:34:38 +0200

    Setting up database on staging

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -27,6 +27,7 @@ $app = new Illuminate\Foundation\Application;
 $env = $app->detectEnvironment(function() {
 
     // Look for an env var called 'ENV' and set the env to that, else default to 'local'
+    var_dump(getenv('ENV'));
     return getenv('ENV') ?: 'development';
 
 });
