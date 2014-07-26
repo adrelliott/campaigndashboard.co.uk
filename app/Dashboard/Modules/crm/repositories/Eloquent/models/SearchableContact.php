a4cb66aa5747eaa11d44dd9974f193515672da3b
Jamie Rumbelow <jamie@jamierumbelow.net>
2014-07-26 11:00:45 +0100

    Adding variants

--- app/Dashboard/Modules/crm/repositories/Eloquent/models/SearchableContact.php
+++ app/Dashboard/Modules/crm/repositories/Eloquent/models/SearchableContact.php
@@ -10,19 +10,16 @@ use Frisk;
 //
 // Here's the syntax I'm using right now. I'll move it into a separate
 // migration or something when I've got the DB structure finalised.
-//
+// 
 // CREATE OR REPLACE VIEW `searchable_contacts` AS
-//     SELECT `contacts`.*, `tags`.`tag_title` AS `tag_name`, concat(`contacts`.`first_name`,' ',`contacts`.`last_name`) AS `full_name`, `order_product`.`product_id` AS `product_id`
+//     SELECT `contacts`.*, `tags`.`tag_title` AS `tag_name`, CONCAT(`tags`.`tag_title`, ' ', `tag_variants`.`variant_name`, ':', `tag_variants`.`variant_value`) AS `tag_with_variants`, concat(`contacts`.`first_name`,' ',`contacts`.`last_name`) AS `full_name`
     
 //     FROM `contacts`
     
 //     -- Tags
 //     LEFT JOIN `contact_tag` ON `contact_tag`.`contact_id` = `contacts`.`id`
 //     LEFT JOIN `tags` on `tags`.`id` = `contact_tag`.`tag_id`
-    
-//     -- Orders
-//     LEFT JOIN `orders` on `orders`.`contact_id` = `contacts`.`id`
-//     LEFT JOIN `order_product` on `order_product`.`order_id` = `orders`.`id`
+//     LEFT JOIN `tag_variants` on `tag_variants`.`tag_id` = `tags`.`id`
 //
 class SearchableContact extends Contact implements Frisk\Searchable
 {
