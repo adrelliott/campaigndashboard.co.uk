25cbab653c5c56204b49a1e9ea1920c08fddc0cb
Jamie Rumbelow <jamie@jamierumbelow.net>
2014-08-11 19:22:28 +0100

    reload the table when you close the modal

--- public/assets/js/app/tables.js
+++ public/assets/js/app/tables.js
@@ -1,17 +1,32 @@
 $(function()
 {
+    var dataTable;
+
+    // Set up the defaults for our tables – these can and will be overridden
+    // in each individual instance of window.dataTableConfig()
     $.extend($.fn.dataTable.defaults, {
         searching: false,
         processing: true
     });
 
+    // Let a view specify a dataTableConfig() function, which, when returning
+    // and object, allows us to configure the table on a per-tenant basis.
     if (typeof window.dataTableConfig == "function")
-        var table = $('.dataTable').DataTable(window.dataTableConfig());
-    else
-        var table = $('.dataTable').DataTable({});
+        dataTable = $('.dataTable').DataTable(window.dataTableConfig());
 
-    function triggerUrl ( url, $tr )
+    // Alternatively, just boot up a datatable instance using the defaults.
+    else
+        dataTable = $('.dataTable').DataTable({});
+
+    // When the user clicks on a row with the class .linked, go to the URL
+    // specified in the row's data.url property, or load it up in a modal.
+    // These properties are set inside the AJAX response in each row's
+    // JSON – look for the DT_RowData parameter in the _row_json.php partials.
+    $(document).on('click', '.dataTable tr.linked', function()
     {
+        var url = $(this).data().url,
+            $tr = $(this);
+
         if ($tr.data('modal'))
         {
             $('#modal').modal('show');
@@ -24,10 +39,12 @@ $(function()
 
         else
             window.location.href = url;
-    }
+    });
 
-    $(document).on('click', '.dataTable tr.linked', function()
+    // When we hide a modal we want to refresh the table, as the user very well
+    // may have updated a row (and we want to reflect that in the table)
+    $('#modal').on('hidden.bs.modal', function()
     {
-        triggerUrl($(this).data().url, $(this));
+        dataTable.draw();
     });
 });
