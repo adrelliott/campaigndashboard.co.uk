62eb5b33c885b2a911a4fc1619ee67d6b8abe4d5
Al Elliott <al@dallasmatthews.co.uk>
2014-09-10 21:42:12 +0200

    debugged the header partials

--- app/views/partials/app/scripts/_footer-staging.blade.php
+++ app/views/partials/app/scripts/_footer-staging.blade.php
@@ -1,18 +1,11 @@
 {{-- The file that cotains the scripts etc for the Development environment --}}
 
-<!-- Call all Frameworks -->
-    {{-- HTML::script('assets/js/bootstrap/wysihtml5-0.3.0_rc2.min.js') --}}
-
+<!-- Get JQuery  -->
+<!-- Moved ot head -->
 
 <!-- Call Core Boostrap JS -->
-{{-- HTML::script('assets/js/bootstrap/ui-bootstrap-tpls-0.10.0.min.js') --}}
-{{ HTML::script('//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js') }}
+{{ HTML::script('assets/js/bootstrap/bootstrap3.0.0.min.js') }}
 
-
-<!-- Call all datatables JS -->
-{{ HTML::script('assets/js/datatables/jquery.dataTables.min.js') }}
-{{ HTML::script('assets/js/datatables/dataTables.bootstrapPagination.js') }}
-{{ HTML::script('assets/js/datatables/dataTables.custom.js') }}
 
 <!-- Call Other Plugins -->
 {{-- HTML::script('assets/js/bootstrap/bootstrap-switch.min.js') --}}
@@ -20,9 +13,29 @@
 {{ HTML::script('assets/js/bootstrap/summernote.min.js') }}
 
 
+<!-- Call all datatables JS -->
+<?php if(!isset($newDatatables)): ?>
+    {{ HTML::script('assets/js/datatables/jquery.dataTables.min.js') }}
+    {{ HTML::script('assets/js/datatables/dataTables.bootstrapPagination.js') }}
+    {{ HTML::script('assets/js/datatables/dataTables.custom.js') }}
+<?php else: ?>
+    {{ HTML::script('assets/js/datatables/jquery.dataTables-1.10.2.min.js') }}
+    {{ HTML::script('assets/js/app/tables.js') }}
+
+    {{ HTML::style('//cdn.datatables.net/plug-ins/725b2a2115b/integration/bootstrap/3/dataTables.bootstrap.css') }}
+    {{ HTML::script('//cdn.datatables.net/plug-ins/725b2a2115b/integration/bootstrap/3/dataTables.bootstrap.js') }}
+<?php endif; ?>
+
 <!-- Call Other JS -->
 {{ HTML::script('assets/js/app/app.js') }}
 {{ HTML::script('assets/js/app/form.js') }}
 {{ HTML::script('assets/js/app/modal.js') }}
-{{-- HTML::script('assets/js/app/appAngular.js') --}}
 
+<h4>Owner id: {{ $owner_id }}</h4>
+<?php /* <h4>Current User</h4>
+{{ dump($current_user) }}
+
+@if(isset($record))
+    <h4>Current record</h4>
+    {{ dump($record) }}
+@endif */ ?>
